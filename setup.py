0f3fd13e5cbc5074db0006b166d96e0367df7c5b
WenyNg <125436484+WenyNg@users.noreply.github.com>
2023-03-12 22:29:42 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup 
+
+setup(
+  name='anfis',
+  version= '0.0.1', 
+  description = 'for fyp use', 
+  url= 'https://github.com/WenyNg/anfis.git',
+  author= 'Ali binti Abu',
+  author_email= 'shuibian123@gmail.com',
+  license= 'unlicense', 
+  packages=['anfis'],
+  zip_safe= False
+)
